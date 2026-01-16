7277bd3eb5e336b98ed4fec34e40692f90e8b3e1
anaha <anaharisohn@gmail.com>
2026-01-16 10:19:10 -0500

    Add Help!

--- King-Anaharishon-a1-Rpg/King-Anaharishon-a1-Rpg/Program.cs
+++ King-Anaharishon-a1-Rpg/King-Anaharishon-a1-Rpg/Program.cs
@@ -14,8 +14,7 @@ int Food;
 float Temp;
 
 Console.WriteLine("Use Help! To see a list of Actions. And Use Stats! To see all your Stats");
-Console.WriteLine();
-Console.WriteLine();
+Console.WriteLine("\n");
 
 YourName:
 Console.WriteLine("It's dark... And cold \nYour Head hurts....");
@@ -32,9 +31,7 @@ else
 
 Console.Clear();
 Console.WriteLine("It's " + Name + "? You think to yourself unsure if it's your name or not");
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine();
+Console.WriteLine("\n\n");
 Console.WriteLine("\"Do you need medical assistants?\" \nYou hear to you're right");
 
 OpenEyes:
@@ -46,11 +43,14 @@ Acttion = Acttion.ToLower();
 Console.WriteLine();
 
 if (Acttion.Contains("y") ^ Acttion.Contains("open"))
-    Console.WriteLine("All you see is white for a few seconds... You see a small red room...\nTo you're right is some kind of Terminal and what looks like a needle is attached to the right of the Terminal.");
+    Console.Clear();
+    Console.WriteLine("All you see is white for a few seconds... You see a small red room...\n");
 else
     goto OpenEyes;
 
-Console.WriteLine("To you're left theres a window andd a chair with someone slumped over. To the right of the person theres a Valve with a label on it\nIn front of you theres what looks to be a vendding machine and a Door");
+
+Room:
+Console.WriteLine("To you're right is some kind of Terminal and what looks like a needle is attached to the right of the it. \nTo you're left theres a window andd a chair with someone slumped over. To the right of the person theres a Valve with a label on it\nIn front of you theres what looks to be a vendding machine and a Door");
 
 Console.WriteLine();
 
@@ -58,3 +58,8 @@ Console.Write("What do you Do? ");
 Acttion = Console.ReadLine();
 
 
+if (Name.Contains("Help!"))
+{
+    Console.WriteLine("\nUse \"Go\" An Then a direction to move\n-Left\n-Right\n-Forword\n-Back \n\nUse \"Look\" And an object to inspect it Or Use \"Around\" to get a list of all interactable objects");
+    goto Room;
+}
--- King-Anaharishon-a1-Rpg/King-Anaharishon-a1-Rpg/obj/Debug/net10.0/King-Anaharishon-a1-Rpg.AssemblyInfo.cs
+++ King-Anaharishon-a1-Rpg/King-Anaharishon-a1-Rpg/obj/Debug/net10.0/King-Anaharishon-a1-Rpg.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("King-Anaharishon-a1-Rpg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54894d81356a3f1865804816819f1d771d4c4dc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c702483cc693ce3ebf57124c778d82628c1d29")]
 [assembly: System.Reflection.AssemblyProductAttribute("King-Anaharishon-a1-Rpg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("King-Anaharishon-a1-Rpg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
